672c18b24f8fd5997ad810a9c95ccf895542b514
DimitrisKapoulas <kapoulasd@gmail.com>
2019-08-31 23:37:43 +0300

    Initialize "fake" data

--- src/main/java/com/dimikaps/Dao/StudentDao.java
+++ src/main/java/com/dimikaps/Dao/StudentDao.java
@@ -1,5 +1,25 @@
 package com.dimikaps.Dao;
 
-public class StudentDao {
+import com.dimikaps.Entity.Student;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class StudentDao {
+    private static Map<Integer, Student> students;
+
+    static {
+        students = new HashMap<Integer, Student>(){
+            {
+                put(1, new Student(1, "Said", "Computer Science"));
+                put(1, new Student(2, "Alex U", "Finance"));
+                put(1, new Student(3, "Anna", "Maths"));
+            }
+        };
+    }
+
+    public Collection<Student> getAllStudents(){
+        return this.students.values();
+    }
 }
